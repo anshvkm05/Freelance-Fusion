99ec8f52031aacc655945b6a8e6681d92ba96725
anshvkm05 <mauryaansh72@gmail.com>
2025-09-08 19:18:23 +0530

    Completed the registration with backend

--- Freelance Fusion/LoginUsercontorls/Register.Designer.cs
+++ Freelance Fusion/LoginUsercontorls/Register.Designer.cs
@@ -33,12 +33,19 @@
             this.EmailTB = new Guna.UI2.WinForms.Guna2TextBox();
             this.PassowordTB = new Guna.UI2.WinForms.Guna2TextBox();
             this.RePasswordTB = new Guna.UI2.WinForms.Guna2TextBox();
-            this.loginbutton = new Guna.UI2.WinForms.Guna2Button();
+            this.RegisterUser = new Guna.UI2.WinForms.Guna2Button();
             this.GoogleLoginBtn = new Guna.UI2.WinForms.Guna2Button();
             this.Githubbtn = new Guna.UI2.WinForms.Guna2Button();
             this.SinginBtn = new Guna.UI2.WinForms.Guna2Button();
             this.Singupbtn = new Guna.UI2.WinForms.Guna2Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblDigit = new System.Windows.Forms.Label();
+            this.lbl8char = new System.Windows.Forms.Label();
+            this.lblCapital = new System.Windows.Forms.Label();
+            this.lblPasswordNotSame = new System.Windows.Forms.Label();
+            this.lblInvalidEmail = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -77,6 +84,7 @@
             this.EmailTB.SelectedText = "";
             this.EmailTB.Size = new System.Drawing.Size(387, 36);
             this.EmailTB.TabIndex = 10;
+            this.EmailTB.Leave += new System.EventHandler(this.EmailTB_Leave);
             // 
             // PassowordTB
             // 
@@ -103,6 +111,9 @@
             this.PassowordTB.SelectedText = "";
             this.PassowordTB.Size = new System.Drawing.Size(387, 36);
             this.PassowordTB.TabIndex = 11;
+            this.PassowordTB.TextChanged += new System.EventHandler(this.PassowordTB_TextChanged);
+            this.PassowordTB.Enter += new System.EventHandler(this.PassowordTB_Enter);
+            this.PassowordTB.Leave += new System.EventHandler(this.PassowordTB_Leave);
             // 
             // RePasswordTB
             // 
@@ -129,24 +140,27 @@
             this.RePasswordTB.SelectedText = "";
             this.RePasswordTB.Size = new System.Drawing.Size(387, 36);
             this.RePasswordTB.TabIndex = 12;
+            this.RePasswordTB.TextChanged += new System.EventHandler(this.RePasswordTB_TextChanged);
+            this.RePasswordTB.Leave += new System.EventHandler(this.RePasswordTB_Leave);
             // 
-            // loginbutton
+            // RegisterUser
             // 
-            this.loginbutton.Animated = true;
-            this.loginbutton.BackColor = System.Drawing.Color.Transparent;
-            this.loginbutton.BorderRadius = 5;
-            this.loginbutton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.loginbutton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.loginbutton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.loginbutton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.loginbutton.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(43)))), ((int)(((byte)(1)))));
-            this.loginbutton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginbutton.ForeColor = System.Drawing.Color.White;
-            this.loginbutton.Location = new System.Drawing.Point(326, 307);
-            this.loginbutton.Name = "loginbutton";
-            this.loginbutton.Size = new System.Drawing.Size(118, 37);
-            this.loginbutton.TabIndex = 13;
-            this.loginbutton.Text = "Register";
+            this.RegisterUser.Animated = true;
+            this.RegisterUser.BackColor = System.Drawing.Color.Transparent;
+            this.RegisterUser.BorderRadius = 5;
+            this.RegisterUser.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.RegisterUser.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.RegisterUser.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.RegisterUser.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.RegisterUser.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(43)))), ((int)(((byte)(1)))));
+            this.RegisterUser.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterUser.ForeColor = System.Drawing.Color.White;
+            this.RegisterUser.Location = new System.Drawing.Point(326, 307);
+            this.RegisterUser.Name = "RegisterUser";
+            this.RegisterUser.Size = new System.Drawing.Size(118, 37);
+            this.RegisterUser.TabIndex = 13;
+            this.RegisterUser.Text = "Register";
+            this.RegisterUser.Click += new System.EventHandler(this.RegisterUser_Click);
             // 
             // GoogleLoginBtn
             // 
@@ -217,15 +231,84 @@
             this.Singupbtn.TabIndex = 17;
             this.Singupbtn.Text = "Sing Up";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lblDigit);
+            this.panel1.Controls.Add(this.lbl8char);
+            this.panel1.Controls.Add(this.lblCapital);
+            this.panel1.Location = new System.Drawing.Point(194, 227);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(196, 51);
+            this.panel1.TabIndex = 30;
+            this.panel1.Visible = false;
+            // 
+            // lblDigit
+            // 
+            this.lblDigit.AutoSize = true;
+            this.lblDigit.ForeColor = System.Drawing.Color.Red;
+            this.lblDigit.Location = new System.Drawing.Point(4, 31);
+            this.lblDigit.Name = "lblDigit";
+            this.lblDigit.Size = new System.Drawing.Size(115, 13);
+            this.lblDigit.TabIndex = 2;
+            this.lblDigit.Text = "At least 1 Number (0-9)";
+            // 
+            // lbl8char
+            // 
+            this.lbl8char.AutoSize = true;
+            this.lbl8char.ForeColor = System.Drawing.Color.Red;
+            this.lbl8char.Location = new System.Drawing.Point(4, 18);
+            this.lbl8char.Name = "lbl8char";
+            this.lbl8char.Size = new System.Drawing.Size(105, 13);
+            this.lbl8char.TabIndex = 1;
+            this.lbl8char.Text = "At least 8 Characters";
+            // 
+            // lblCapital
+            // 
+            this.lblCapital.AutoSize = true;
+            this.lblCapital.ForeColor = System.Drawing.Color.Red;
+            this.lblCapital.Location = new System.Drawing.Point(4, 5);
+            this.lblCapital.Name = "lblCapital";
+            this.lblCapital.Size = new System.Drawing.Size(162, 13);
+            this.lblCapital.TabIndex = 0;
+            this.lblCapital.Text = "At least 1 Uppercase Letter (A-Z)";
+            // 
+            // lblPasswordNotSame
+            // 
+            this.lblPasswordNotSame.AutoSize = true;
+            this.lblPasswordNotSame.ForeColor = System.Drawing.Color.Red;
+            this.lblPasswordNotSame.Location = new System.Drawing.Point(481, 300);
+            this.lblPasswordNotSame.Name = "lblPasswordNotSame";
+            this.lblPasswordNotSame.Size = new System.Drawing.Size(99, 13);
+            this.lblPasswordNotSame.TabIndex = 3;
+            this.lblPasswordNotSame.Text = "Password not same";
+            this.lblPasswordNotSame.Visible = false;
+            // 
+            // lblInvalidEmail
+            // 
+            this.lblInvalidEmail.AutoSize = true;
+            this.lblInvalidEmail.ForeColor = System.Drawing.Color.Red;
+            this.lblInvalidEmail.Location = new System.Drawing.Point(196, 153);
+            this.lblInvalidEmail.Name = "lblInvalidEmail";
+            this.lblInvalidEmail.Size = new System.Drawing.Size(66, 13);
+            this.lblInvalidEmail.TabIndex = 31;
+            this.lblInvalidEmail.Text = "Invalid Email";
+            this.lblInvalidEmail.Visible = false;
+            this.lblInvalidEmail.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblInvalidEmail);
+            this.Controls.Add(this.lblPasswordNotSame);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.Singupbtn);
             this.Controls.Add(this.SinginBtn);
             this.Controls.Add(this.Githubbtn);
             this.Controls.Add(this.GoogleLoginBtn);
-            this.Controls.Add(this.loginbutton);
+            this.Controls.Add(this.RegisterUser);
             this.Controls.Add(this.RePasswordTB);
             this.Controls.Add(this.PassowordTB);
             this.Controls.Add(this.EmailTB);
@@ -233,7 +316,10 @@
             this.Name = "Register";
             this.Size = new System.Drawing.Size(770, 516);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -243,10 +329,16 @@
         private Guna.UI2.WinForms.Guna2TextBox EmailTB;
         private Guna.UI2.WinForms.Guna2TextBox PassowordTB;
         private Guna.UI2.WinForms.Guna2TextBox RePasswordTB;
-        private Guna.UI2.WinForms.Guna2Button loginbutton;
+        private Guna.UI2.WinForms.Guna2Button RegisterUser;
         private Guna.UI2.WinForms.Guna2Button GoogleLoginBtn;
         private Guna.UI2.WinForms.Guna2Button Githubbtn;
         private Guna.UI2.WinForms.Guna2Button SinginBtn;
         private Guna.UI2.WinForms.Guna2Button Singupbtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblDigit;
+        private System.Windows.Forms.Label lbl8char;
+        private System.Windows.Forms.Label lblCapital;
+        private System.Windows.Forms.Label lblPasswordNotSame;
+        private System.Windows.Forms.Label lblInvalidEmail;
     }
 }
--- Freelance Fusion/LoginUsercontorls/Register.cs
+++ Freelance Fusion/LoginUsercontorls/Register.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,15 +17,161 @@ namespace Freelance_Fusion.LoginUsercontorls
 {
     public partial class Register : UserControl
     {
+        string email;
+        string password;
+        string confirmPassword;
+
+        // --- CORRECTED HERE: Removed the hidden '\r\n' characters from all strings ---
+        private const string FirebaseApiKey = "AIzaSyB0JEuC9PiZle1u7yk2HoyIPNCcUllZjNY"; // Paste your key here
+        private const string FirebaseAuthDomain = "freelancefusion-30sep.firebaseapp.com"; // Get this from Firebase Auth settings
+        private const string FirebaseDatabaseUrl = "https://freelancefusion-30sep-default-rtdb.firebaseio.com/"; // From Realtime Database
+
+        // --- CORRECTED HERE: The main client object for authentication ---
+        private readonly FirebaseAuthClient authClient;
+        private readonly FirebaseClient firebaseClient;
         public Register()
         {
             InitializeComponent();
+            // --- CORRECTED HERE: Use 'FirebaseAuthConfig' and 'FirebaseAuthClient' ---
+            var config = new FirebaseAuthConfig
+            {
+                ApiKey = FirebaseApiKey,
+                AuthDomain = FirebaseAuthDomain,
+                // The library automatically finds the right providers
+                Providers = new FirebaseAuthProvider[]
+                {
+                    // Add more providers here if you need them
+                    new EmailProvider()
+                }
+            };
+
+            authClient = new FirebaseAuthClient(config);
+            firebaseClient = new FirebaseClient(FirebaseDatabaseUrl);
         }
         public event EventHandler SigninbtnClick;
 
         private void SinginBtn_Click(object sender, EventArgs e)
         {
             SigninbtnClick?.Invoke(this, EventArgs.Empty);
+        }
+        private bool IsValidEmail(string email)
+        {
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, emailPattern);
+        }
+        private async void RegisterUser_Click(object sender, EventArgs e)
+        {
+            email = EmailTB.Text.Trim();
+            password = PassowordTB.Text;
+            confirmPassword = RePasswordTB.Text;
+
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Please enter both email and password.");
+                return;
+            }
+
+            if (!IsValidEmail(email))
+            {
+                MessageBox.Show("Please enter a valid email address.");
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Passwords do not match.");
+                return;
+            }
+
+            try
+            {
+                // This is the core Firebase call to create a new user
+                await authClient.CreateUserWithEmailAndPasswordAsync(email, password);
+
+                MessageBox.Show("Registration Successful! You can now sign in.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // You could automatically switch to the sign-in panel here if you want
+                SigninbtnClick?.Invoke(this, EventArgs.Empty);
+            }
+            catch (FirebaseAuthException ex)
+            {
+                // This catches errors from Firebase, like "email already in use"
+                MessageBox.Show($"Registration Failed: {ex.Reason}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                // This catches other errors, like no internet connection
+                MessageBox.Show($"An unexpected error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void PassowordTB_TextChanged(object sender, EventArgs e)
+        {
+            string password = PassowordTB.Text;
+
+            // Check for at least one uppercase letter
+            if (Regex.IsMatch(password, @"[A-Z]"))
+                lblCapital.ForeColor = Color.Green;
+            else
+                lblCapital.ForeColor = Color.Red;
+
+            // Check for at least 8 characters
+            if (password.Length >= 8)
+                lbl8char.ForeColor = Color.Green;
+            else
+                lbl8char.ForeColor = Color.Red;
+
+            // Check for at least one digit
+            if (Regex.IsMatch(password, @"\d"))
+                lblDigit.ForeColor = Color.Green;
+            else
+                lblDigit.ForeColor = Color.Red;
+        }
+        private void PassowordTB_Enter(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+        }
+        private void PassowordTB_Leave(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void RePasswordTB_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EmailTB_Leave(object sender, EventArgs e)
+        {
+            string email = EmailTB.Text.Trim();
+            if (!IsValidEmail(email))
+            {
+                lblInvalidEmail.Visible = true;
+                return;
+            }
+            else
+            {
+                lblInvalidEmail.Visible = false;
+            }
+        }
+
+        private void RePasswordTB_TextChanged(object sender, EventArgs e)
+        {
+            password = PassowordTB.Text;
+            confirmPassword = RePasswordTB.Text;
+            if (password != confirmPassword)
+            {
+                lblPasswordNotSame.Visible = true;
+                return;
+            }
+            else
+            {
+                lblPasswordNotSame.Visible = false;
+            }
         }
     }
 }
