aa9c2e05596c2de9b409e8baad56803c1bda3681
anshvkm05 <mauryaansh72@gmail.com>
2025-09-22 22:33:02 +0530

    started with freelancer questionaries and client questionaries form

---
+++ Freelance Fusion/FreelancerClientDetailsEnter/ClientQuestionaries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Freelance_Fusion.FreelancerClientDetailsEnter
+{
+    public partial class ClientQuestionaries : UserControl
+    {
+        public ClientQuestionaries()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Freelance Fusion/FreelancerClientDetailsEnter/Freelancer_questionaries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Freelance_Fusion.FreelancerClientDetailsEnter
+{
+    public partial class Freelancer_questionaries : UserControl
+    {
+        public Freelancer_questionaries()
+        {
+            InitializeComponent();
+        }
+    }
+}
